faeb9506333948ab47c38fe2f810a45664404142
Vitor Balocco <vitorbal@gmail.com>
2016-07-05 21:40:09 +0200

    Fix: add node_modules to env.path before running jscodeshift (fixes #2)

--- bin/eslint-transforms.js
+++ bin/eslint-transforms.js
@@ -2,15 +2,44 @@
 
 "use strict";
 
-var nodeCLI = require("shelljs-nodecli");
+var execSync = require("child_process").execSync;
+var objectAssign = require("object-assign");
 var path = require("path");
 
 var argv = process.argv.slice(2);
 var args = argv.slice(1);
 var transform = argv[0];
 
-nodeCLI.exec(
+/**
+ * Add possible node_modules/.bin paths to env and run the command passed in.
+ *
+ * @param {string} cmd The command to run
+ * @returns {void}
+ */
+function execWithNodeModules(cmd) {
+    var SEPARATOR = process.platform === "win32" ? ";" : ":",
+        env = objectAssign({}, process.env);
+
+    env.PATH = [
+        // Covers case when npm flattens dependencies and the jscodeshift bin will be directly under the root
+        // node_modules folder
+        path.resolve("./node_modules/.bin"),
+        // Covers case when dependencies are not flattened and the jscodeshift bin can be found under the
+        // node_modules folder of our package
+        path.resolve(__dirname, "../node_modules/.bin"),
+        env.PATH
+    ].join(SEPARATOR);
+
+    execSync(cmd, {
+        env: env,
+        cwd: process.cwd(),
+        stdio: "inherit"
+    });
+}
+
+execWithNodeModules([
     "jscodeshift",
-    "-t", path.resolve(__dirname, "../lib/" + transform + "/" + transform + ".js"),
+    "-t",
+    path.resolve(__dirname, "../lib/" + transform + "/" + transform + ".js"),
     args.join(" ")
-);
+].join(" "));
