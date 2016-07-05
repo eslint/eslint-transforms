8d9647045e973e4a5443e055f425bfb0f0ca4665
Vitor Balocco <vitorbal@gmail.com>
2016-07-05 19:02:03 +0200

    Fix: use __dirname to find transform files (refs #2)

--- bin/eslint-transforms.js
+++ bin/eslint-transforms.js
@@ -3,6 +3,7 @@
 "use strict";
 
 var nodeCLI = require("shelljs-nodecli");
+var path = require("path");
 
 var argv = process.argv.slice(2);
 var args = argv.slice(1);
@@ -10,6 +11,6 @@ var transform = argv[0];
 
 nodeCLI.exec(
     "jscodeshift",
-    "-t", "./lib/" + transform + "/" + transform + ".js",
+    "-t", path.resolve(__dirname, "../lib/" + transform + "/" + transform + ".js"),
     args.join(" ")
 );
