b16fd613bf258eae061eabc984b9926aa4e46b97
Nitin Kumar <nitin.kumar@razorpay.com>
2024-05-22 19:44:44 +0530

    chore: add test cases

--- tests/lib/v9-migration/v9-migration.js
+++ tests/lib/v9-migration/v9-migration.js
@@ -6,9 +6,10 @@
 
 "use strict";
 
+const path = require("path");
 const { applyTransform } = require("@hypermod/utils");
 const assert = require("assert");
-
+const sinon = require("sinon");
 const v9MigrationTransform = require("../../../lib/v9-migration/v9-migration");
 
 describe("v9 migration transform", () => {
@@ -57,44 +58,71 @@ describe("v9 migration transform", () => {
             const nodeByRangeIndex = context.sourceCode.getNodeByRangeIndex();
             const commentsBefore = context.sourceCode.getCommentsBefore(nodeOrToken);
             const commentsAfter = context.sourceCode.getCommentsAfter(nodeOrToken);
+            const commentsInside = context.sourceCode.getCommentsInside(nodeOrToken);
             `.trim()
         );
     });
 
-    it.only("should warn about codePath.currentSegments", async () => {
+    it("should warn about context.getComments()", async () => {
+        const spy = sinon.spy(console, "warn");
+
         await applyTransform(
             v9MigrationTransform,
-            `
-            module.exports = {
-                meta: {
-                    docs: {},
-                    schema: []
-                },
-                create(context) {
-                    return {
-                        onCodePathStart(codePath, node) {
-                            const currentSegments = codePath.currentSegments();
-                        },
-                        
-                        onCodePathEnd(endCodePath, node) {
-                            const currentSegments = endCodePath.currentSegments();
-                        },
-                    };
-                }
+            {
+                source: "const comments = context.getComments();",
+                path: path.resolve(__dirname, __filename)
             }
-            `
         );
 
-        // assert.strictEqual(
-        //     result,
-        //     `
-        //     const sourceCode = context.sourceCode.getText();
-        //     const sourceLines = context.sourceCode.getLines();
-        //     const allComments = context.sourceCode.getAllComments();
-        //     const nodeByRangeIndex = context.sourceCode.getNodeByRangeIndex();
-        //     const commentsBefore = context.sourceCode.getCommentsBefore(nodeOrToken);
-        //     const commentsAfter = context.sourceCode.getCommentsAfter(nodeOrToken);
-        //     `.trim()
-        // );
+        assert.strictEqual(spy.callCount, 1);
+        assert.match(
+            spy.args[0][0],
+            /1:17 The "getComments\(\)" method has been removed. Please use "getCommentsBefore\(\)", "getCommentsAfter\(\)", or "getCommentsInside\(\)" instead/u
+        );
+
+        spy.restore();
+    });
+
+    it("should warn about codePath.currentSegments", async () => {
+        const spy = sinon.spy(console, "warn");
+        const filePath = path.resolve(__dirname, __filename);
+
+        await applyTransform(
+            v9MigrationTransform,
+            {
+                path: filePath,
+                source: `
+                module.exports = {
+                    meta: {
+                        docs: {},
+                        schema: []
+                    },
+                    create(context) {
+                        return {
+                            onCodePathStart(codePath, node) {
+                                const currentSegments = codePath.currentSegments();
+                            },
+                            
+                            onCodePathEnd(endCodePath, node) {
+                                const currentSegments = endCodePath.currentSegments();
+                            },
+                        };
+                    }
+                }
+                `
+            }
+        );
+
+        assert.strictEqual(spy.callCount, 2);
+        assert.match(
+            spy.args[0][0],
+            /10:56 The "CodePath#currentSegments" property has been removed and it can't be migrated automatically/u
+        );
+        assert.match(
+            spy.args[1][0],
+            /14:56 The "CodePath#currentSegments" property has been removed and it can't be migrated automatically/u
+        );
+
+        spy.restore();
     });
 });
