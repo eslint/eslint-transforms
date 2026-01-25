57053bb245bca7bbeb5af4d77396df406f66578e
루밀LuMir <rpfos@naver.com>
2026-01-25 14:51:06 +0900

    chore: introduce `prettier`

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,11 @@
 
 const { defineConfig, globalIgnores } = require("eslint/config");
 const eslintConfigESLintCJS = require("eslint-config-eslint/cjs");
-const eslintConfigESLintFormatting = require("eslint-config-eslint/formatting");
 const globals = require("globals");
 
 module.exports = defineConfig([
     globalIgnores(["tests/fixtures/"]),
     eslintConfigESLintCJS,
-    eslintConfigESLintFormatting,
     {
         files: ["tests/**/*"],
         languageOptions: {
